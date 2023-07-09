ee0f8cdc0e057e1d90dd4c0040a1ee5b3674228f
jaimin1208 <jaiminpanchal1208@gmail.com>
2023-07-09 19:47:08 +0530

    second commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Navbar from './components/Navbar'
-import Home from './pages/Home/Home'
-import About from './pages/about/About'
-import Themes from "./components/Themes"
-import Portfolio from './pages/portfolio/Portfolio'
-import Contact from './pages/contact/Contact'
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Themes from "./components/Themes";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Contact from "./pages/contact/Contact";
 
 function App() {
-  
-
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Themes />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Themes />
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="portfolio" element={<Portfolio />} />
+				<Route path="contact" element={<Contact />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
